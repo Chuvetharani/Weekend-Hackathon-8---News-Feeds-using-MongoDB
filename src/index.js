f761ab307bf780a7329a620012e0458c1b9f7cbb
Chuvetharani <68391363+Chuvetharani@users.noreply.github.com>
2020-12-15 18:58:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { newsArticleModel } = require("./connector");
 const app = express()
 const port = 8080
 
@@ -11,9 +12,7 @@ app.use(express.json());
 app.get('/newFeeds', (req, res) => {
     let limit = isNaN(req.query.limit) || !req.query.limit ? 10 : parseInt(req.query.limit);
     let offset = isNaN(req.query.offset) || !req.query.offset ? 0 : parseInt(req.query.offset);
-    newsArticleModel.find().limit(limit).skip(offset).then((result) => {
-        res.send(result);
-    });
+    newsArticleModel.find({}).limit(limit).skip(offset).then((result) => res.json(result)).catch((err) => res.status(400).send(err));
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
